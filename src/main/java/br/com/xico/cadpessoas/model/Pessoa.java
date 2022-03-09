378e1eaa1d697e93567c43da151f72588962a2f9
PWeb UFERSA <pwebufersa@gmail.com>
2022-03-09 11:49:03 -0300

    Add Cad Pessoas Incompleto

---
+++ src/main/java/br/com/xico/cadpessoas/model/Pessoa.java
@@ -0,0 +1,43 @@
+package br.com.xico.cadpessoas.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Xico
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "pessoas")
+public class Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 8128389568127787766L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+	private String email;
+	private String genero;
+
+	@Column(nullable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dataNascimento;
+
+}
