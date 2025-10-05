d06ffb3ed1c5319e81cb2942579f4097c57dd004
Xico <xico@ufersa.edu.br>
2025-10-05 15:31:08 -0300

    Remove Lombok and add explicit methods to Pessoa
    Lombok dependency and related plugin configuration were removed from pom.xml. The Pessoa model now includes explicit constructors, getters, setters, equals, hashCode, and toString methods. Updated banner.txt ASCII art and revised sobre.html to reference 2025.2 instead of 2021.2.

--- src/main/java/br/com/xico/cadpessoas/model/Pessoa.java
+++ src/main/java/br/com/xico/cadpessoas/model/Pessoa.java
@@ -12,16 +12,11 @@ import javax.persistence.Table;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 /**
  * @author Xico
  */
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Entity
 @Table(name = "pessoas")
 public class Pessoa implements Serializable {
@@ -40,4 +35,77 @@ public class Pessoa implements Serializable {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate dataNascimento;
 
+	public Pessoa() {}
+
+	public Pessoa(Long id, String nome, String email, String genero, LocalDate dataNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.genero = genero;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Pessoa pessoa = (Pessoa) o;
+		return id != null ? id.equals(pessoa.id) : pessoa.id == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa{" +
+				"id=" + id +
+				", nome='" + nome + '\'' +
+				", email='" + email + '\'' +
+				", genero='" + genero + '\'' +
+				", dataNascimento=" + dataNascimento +
+				'}';
+	}
 }
