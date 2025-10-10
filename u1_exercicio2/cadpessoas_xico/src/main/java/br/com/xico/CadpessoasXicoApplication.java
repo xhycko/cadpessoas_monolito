e627123080887bb6b4f033055c065483c38ca3f8
Xico <xico@ufersa.edu.br>
2025-10-10 20:11:41 -0300

    u1 exercicio 2 - inicio

---
+++ u1_exercicio2/cadpessoas_xico/src/main/java/br/com/xico/CadpessoasXicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.xico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasXicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasXicoApplication.class, args);
+	}
+
+}
